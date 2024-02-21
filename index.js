5965d358ed724e8fc30286f0953fa421c624b98e
PLUGPHUWADON <ppuwdon@gmail.com>
2024-02-21 09:47:02 +0700

    c

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 // set db url
-const dbUrl = 'postgres://webadmin:BXNxiq80565@node57004-phuwadon-noderest.proen.app.ruk-com.cloud:11799/books';
+const dbUrl = 'postgres://webadmin:BXNxiq80565@node57004-phuwadon-noderest.proen.app.ruk-com.cloud/books';
 
 // create a connection to the database
 const sequelize = new Sequelize(dbUrl);
